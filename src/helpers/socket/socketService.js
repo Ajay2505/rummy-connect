78b767be689d616b53c314a82e09ae11981b3163
Ajay2505 <ajaykumarsanju2505@gmail.com>
2024-10-09 00:27:05 +0530

    a

--- src/helpers/socket/socketService.js
+++ src/helpers/socket/socketService.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 import io from 'socket.io-client';
 
 const socket = io(process.env.REACT_APP_BACKENDURL, {
-    path: '/server', // Ensure this matches the backend Socket.io path
+    path: '/server/ws', // Ensure this matches the backend Socket.io path
     secure: true,    // Use secure connection
     // transports: ['websocket'], // Force WebSocket transport
 });
